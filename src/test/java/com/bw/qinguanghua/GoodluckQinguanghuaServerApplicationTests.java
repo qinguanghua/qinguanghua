b51a1fbfdab054374e3209f03741fb70b488adeb
qin <1275144003@qq.com>
2017-07-28 18:31:18 +0800

    server

---
+++ src/test/java/com/bw/qinguanghua/GoodluckQinguanghuaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bw.qinguanghua;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoodluckQinguanghuaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
